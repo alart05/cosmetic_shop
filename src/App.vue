a2273582f1d327bc6a70dd175bd2856685e14a11
alart05 <88630187+alart05@users.noreply.github.com>
2021-11-28 21:29:52 +0300

    магазин косметики

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,151 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page">
+    <div>
+      <div class='block1'>
+        <div class='block1__header'>Alina’s shop</div>
+        <div class='section1__buttons'>
+          <div><button class='section1__button' @click="loadDataBlush()">Blush</button></div>
+          <div><button class='section1__button' @click="loadDataBronzer()">Bronzer</button></div>
+        </div>
+      </div>
+    </div>
+    <div class='block2'>
+      <div class='section2'> 
+        <div class='section2__text'>Filter’s</div>
+        <div class='hypoallergenic'>Hypoallergenic</div>
+        <div class='hypoallergenic'>Hypoallergenic</div>
+        <div class='hypoallergenic'>Hypoallergenic</div>
+      </div>
+      <div class='cards'>
+        <Card @click.native="showNewPage = true"
+          v-for="product in products" :key="product.id" 
+          :item="product"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Card from '@/components/Card.vue'
+import axios from 'axios'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    this.loadDataBlush()
+  },
+  methods: {
+    loadDataBronzer() {
+      this.products.splice(0, this.products.length)
+      axios({
+        method: 'get',
+        url: 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=bronzer'  
+      })
+        .then((response) => {
+          console.log(response)
+          this.products.push(...response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    loadDataBlush() {
+      this.products.splice(0, this.products.length)
+      axios({
+        method: 'get',
+        url: 'http://makeup-api.herokuapp.com/api/v1/products.json?product_type=blush'  
+      })
+        .then((response) => {
+          console.log(response)
+          this.products.push(...response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+  .page{
+    display: grid;
+    grid-template-columns: 200px 1fr;
+    padding: 35px;
+  }
+  .section1{
+    color: #2264D1;
+    font-size: 24px;
+  }
+  .block1{
+    display: flex;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+    margin-top: 40px;
+    padding-right: 13px;
+    position: sticky;
+    top: 45px;
+  }
+  .block1__header{
+    color: #2264D1;
+    font-weight: bold;
+    font-size: 24px;
 }
+  .section1__button{
+    margin-top: 18px;
+    border: 1px solid #9DC2FF;
+    box-sizing: border-box;
+    border-radius: 8px;
+    width: 187px;
+    height: 49px;
+    color: #2264D1;
+    font-weight: bold;
+    font-size: 15px;
+    cursor: pointer;
+  }
+  .section1__button:hover {
+    box-shadow: 6px 24px 34px rgb(0 0 0 / 10%), 3px 9px 14px rgb(0 0 0 / 9%);
+}
+  .section1__buttons{
+    padding-top: 35px;
+  }
+  .section1{
+    display: grid;
+
+  }
+  .hypoallergenic{
+    background: #EDEDF0;
+    border-radius: 99px;
+    width: 120px;
+    height: 32px;
+    color: #4A4B57;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    padding-top: 12px;
+    padding-left: 4px;
+    margin: 0px 12px;
+    cursor: pointer;
+  }
+  .section2{
+    display: flex;
+  }
+  .section2__text{
+    padding-top: 13px;
+  }
+  .cards{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 40px;
+    padding-top: 40px;
+  }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class='card'>
+    <img class="image" :src='item.image_link'>
+    <div class='text'>{{ item.name }}</div>
+    <div class='price'>${{ item.price }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    item: { type: Object }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .image{
+    width: 100%;
+    margin: auto;  
+    display: flex;
+  }
+  .text{
+    text-align: center;
+    color: #19191D;
+  }
+  .card{
+    border: 1px solid #9DC2FF;
+    box-sizing: border-box;
+    border-radius: 10px;
+    padding-bottom: 10px;
+    cursor: pointer;
+  }
+  .card:hover {
+    box-shadow: 6px 24px 34px rgb(0 0 0 / 10%), 3px 9px 14px rgb(0 0 0 / 9%);
+}
+  .button{
+    border: 1px solid #9DC2FF;
+    box-sizing: border-box;
+    border-radius: 8px;
+    width: 88px;
+    height: 32px;
+    color: #2264D1;
+  }
+  .price{
+    margin-top: 7px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 24px;
+    color:#000000;
+    padding-top: 10px;
+  }
+</style>
